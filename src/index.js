93a9fca89c6b4baf219bed4953e58cbe306bfbe9
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-07-01 16:04:44 +0900

    Simplify loading

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import { CSVGenerator } from './core/CSVGenerator.js';
 import { ICSGenerator } from './core/ICSGenerator.js';
 import { TABLoader } from './tab/TABLoader.js';
+import { TABUrlRetriever } from './tab/TABUrlRetriever.js';
 
 ( async () => {
 
-    const loader = new TABLoader();
-    const results = await loader.load( 'https://www.tokyoartbeat.com/_next/data/OICSeXKMliEAJd-Er-5E2/en/events/orderBy/latest.json?q=orderBy&q=latest' );
+    const url = await new TABUrlRetriever().load();
+    const results = await new TABLoader().load( url );
 
-    // const gen = new CSVGenerator();
-    // console.log( gen.generate( results ) );
-
-    const gen = new ICSGenerator();
-    console.log( await gen.generate( results ) );
+    // console.log( new CSVGenerator().generate( results ) );
+    console.log( await new ICSGenerator().generate( results ) );
 
 } )();
---
+++ src/tab/TABUrlRetriever.js
@@ -0,0 +1,51 @@
+import puppeteer from 'puppeteer';
+
+export class TABUrlRetriever {
+
+    async load() {
+
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        await page.setRequestInterception( true );
+
+        let url = null;
+        return new Promise( resolve => {
+
+            page.on( 'request', request => {
+
+                if ( /latest\.json/.test( request.url() ) ) {
+
+                    url = request.url();
+                    resolve();
+
+                } else {
+
+                    request.continue();
+
+                }
+
+            } );
+
+            page.goto( 'https://www.tokyoartbeat.com/en/events', {
+                waitUntil: 'domcontentloaded',
+                timeout: 5000
+            } );
+
+        } ).then( () => {
+
+            return page.close();
+
+        } ).then( () => {
+
+            return browser.close();
+
+        } ).then( () => {
+
+            return url;
+
+        } );
+
+    }
+
+}
